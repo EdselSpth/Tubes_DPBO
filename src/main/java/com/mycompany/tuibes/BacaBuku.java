a8921278e8d35b19bfeb40e51953edddc1047bc4
tifunnyy <159507386+Funny-pixel765@users.noreply.github.com>
2024-12-08 18:15:01 +0700

    Update BacaBuku.java

--- src/main/java/com/mycompany/tuibes/BacaBuku.java
+++ src/main/java/com/mycompany/tuibes/BacaBuku.java
@@ -1,21 +1,28 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.tuibes;
 
 /**
  *
  * @author りおん塩田
- */
-public class BacaBuku {
+ */public class BacaBuku {
     private String tanggalTerakhirBaca;
-    
-    public void bacaBuku(){
-        
+
+    public void bacaBuku() {
+        System.out.println("Membaca buku...");
     }
-    
-    public void searchBuku(){
-        
+
+    public void searchBuku() {
+        System.out.println("Mencari buku...");
+    }
+
+    public void setTerakhirBaca(String tanggal) {
+        this.tanggalTerakhirBaca = tanggal;
+    }
+
+    public String getTanggalTerakhirBaca() {
+        return this.tanggalTerakhirBaca;
     }
 }
+
