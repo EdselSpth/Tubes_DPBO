d398f17f061de7d476442920b75ed12a8228efad
tifunnyy <159507386+Funny-pixel765@users.noreply.github.com>
2024-12-08 18:45:11 +0700

    Update Tuibes.java

--- src/main/java/com/mycompany/tuibes/Tuibes.java
+++ src/main/java/com/mycompany/tuibes/Tuibes.java
@@ -11,6 +11,19 @@ package com.mycompany.tuibes;
 public class Tuibes {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Pembelian pembelian = new Pembelian();
+        pembelian.setIDPembelian("OGHU112");
+        String tanggalPembelian = "2020-10-20";
+        pembelian.showStruk();
+        int totalHarga = pembelian.totalHarga();
+        System.out.println("Total harga dari buku: " + totalHarga);
+
+        BacaBuku bacaBuku = new BacaBuku();
+        bacaBuku.bacaBuku();
+        bacaBuku.searchBuku();
+        bacaBuku.setTerakhirBaca("15 Januari 2021");
+        String terakhirBacca = bacaBuku.getTanggalTerakhirBaca();
+        System.out.println("Tanggal terakhir baca buku. . . :" + bacaBuku);
+        
     }
 }
