bdf8d140ac785ea1d5e67c0879cfd919c6e3912c
Raditya <mp.raditya93w@gmail.com>
2024-12-11 21:41:45 +0700

    perpustakaan commit

--- src/main/java/com/mycompany/tuibes/Perpustakaan.java
+++ src/main/java/com/mycompany/tuibes/Perpustakaan.java
@@ -8,18 +8,43 @@ package com.mycompany.tuibes;
  *
  * @author りおん塩田
  */
+import java.util.ArrayList;
+
 public class Perpustakaan {
-    private Book[] listBuku;
+    private ArrayList<Book> listBuku;
+    
+    public Perpustakaan() {
+        this.listBuku = new ArrayList<>();
+    }
     
     public void showBuku(){
-        System.out.println("EEK");
+        if (listBuku.isEmpty()) {
+            System.out.println("Tidak Ada Buku di perpustakaan");
+        }else {
+            for (Book buku : listBuku) {
+                buku.printInfoBuku();
+                System.out.println("-----------------------");
+            }
+        }
     }
     
-    public void tambahBuku(){
-        
+    public void tambahBuku(Book bukuBaru){
+        listBuku.add(bukuBaru);
+        System.out.println("Buku \"" + bukuBaru.getJudulBuku() + "\" berhasil ditambahkan.");
     }
     
-    public void refundBuku(){
-        
+    public void refundBuku(String idBuku){
+        boolean ditemukan = false;
+        for (Book buku : listBuku) {
+            if (buku.getIdBuku().equals(idBuku)) {
+                listBuku.remove(buku);
+                System.out.println("Buku dengan ID \"" + idBuku + "\" berhasil dihapus.");
+                ditemukan = true;
+                break;
+            }
+        }
+        if (!ditemukan) {
+            System.out.println("Buku dengan ID \"" + idBuku + "\" tidak ditemukan.");
+        }
     }
 }
