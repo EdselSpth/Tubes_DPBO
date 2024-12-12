de13ed3f57459281922bc0528e570221f544506c
EdselSpth <edselspth@gmail.com>
2024-12-12 09:02:44 +0700

    making package separate and adding fuction

--- src/main/java/com/mycompany/account/Account.java
+++ src/main/java/com/mycompany/account/Account.java
@@ -11,11 +11,29 @@ import java.util.Scanner;
  * @author りおん塩田
  */
 public class Account {
-    Scanner s = new Scanner(System.in);
-    Admin A = new Admin();
-    
-    
-    public void Login(){
-        protected username;
+    private String username;
+    private String password;
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+   
+    
 }
--- src/main/java/com/mycompany/account/Admin.java
+++ src/main/java/com/mycompany/account/Admin.java
@@ -3,58 +3,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.account;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  *
  * @author りおん塩田
  */
 public class Admin  {
-    Scanner s = new Scanner(System.in);
-   
+    ArrayList<Account> accountList = new ArrayList<>();
     
-    public void adminMenuUsing(){
-        System.out.print("Pilih Menu : ");
-        int pilihan = s.nextInt();
-        switch(pilihan){
-            case 1:
-                menuKategoriBuku();
-                tambahBuku();
-            case 2:
-            
-            case 3:
-            
-            case 4:
-                
-            default:
-                System.out.println("Pilihan Menu Tidak Ada");
-        }
-        
-    }
-    
-    public void tambahBuku(){
-        System.out.println("");
-        menuKategoriBuku();
-        System.out.print("Pilih  Kategori Buku : ");
-        int pilihan = s.nextInt();
-        switch (pilihan) {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            default:
-                System.out.println("Pilihan Menu Tidak Tersedia");;
-        }
-        
-    }
+    accountList.add(new Account("Admin", "Admin123"));
     
     
 }
